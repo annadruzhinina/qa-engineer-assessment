67c233320127e35620e34e6e51150fdff1a40a3e
anna <anna@rcbd.org>
2024-10-03 11:18:55 -0400

    Refactored tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, fireEvent, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';  // Provides additional matchers like toBeInTheDocument
 import App from './App';  // Adjust the path if needed
+import { Todo } from './interface';
+
 
 // Mock localStorage for testing purposes
 beforeEach(() => {
@@ -37,23 +39,23 @@ afterEach(() => {
 
 // Toggle Checked State: Clicking on a todo item should toggle the “checked” state.
 test('toggling todo item changes its checked state', () => {
-    const { getByLabelText } = render(<App />);
-    const todoItem = getByLabelText(/Buy groceries/i);
+    render (<App />);
+    let todoItem = screen.getByLabelText(/Buy groceries/i);
 
     // Initially unchecked
     expect(todoItem).not.toBeChecked();
 
     // Toggle checkbox
     fireEvent.click(todoItem);
-
+    todoItem = screen.getByLabelText(/Buy groceries/i);
     // Expect it to be checked
     expect(todoItem).toBeChecked();
   });
 
   // Toggle Unchecked State: Clicking on a todo item should toggle the “unchecked” state.
   test('toggling todo item changes its checked state', () => {
-    const { getByLabelText } = render(<App />);
-    const todoItem = getByLabelText(/Ace CoderPad interview/i);
+    render(<App />);
+    const todoItem = screen.getByLabelText(/Ace CoderPad interview/i);
 
     // Initially unchecked
     expect(todoItem).toBeChecked();
@@ -121,22 +123,23 @@ test('checked items sink to the bottom of the list automatically', () => {
   // Get all list items before any interaction
   let listItems = screen.getAllByRole('listitem');
 
-  console.log('listItems: ', listItems)
+  // console.log('listItems: ', listItems)
 
   // Verify initial order of items
-  expect(listItems[0]).toHaveTextContent('Test 1');
-  expect(listItems[1]).toHaveTextContent('Test 2');
-  expect(listItems[2]).toHaveTextContent('Test 3');
+    expect(listItems[0]).toHaveTextContent('Test 1');
+    expect(listItems[1]).toHaveTextContent('Test 2');
+    expect(listItems[2]).toHaveTextContent('Test 3');
 
 
     // Simulate checking the second item ("Reboot computer")
     const item1Checkbox = screen.getByLabelText('Test 1');
     fireEvent.click(item1Checkbox);
-
+    // console.log('listItems - after click: ', listItems)
     // Get all list items after checking the item
     listItems = screen.getAllByRole('listitem');
 
-    console.log('listItems - after click: ', listItems)
+    // Check the count again after interaction
+    expect(listItems).toHaveLength(3);
 
     // Verify that "Reboot computer" has moved to the bottom
     expect(listItems[0]).toHaveTextContent('Test 2');
